af2573433d4bfaf883c3a5addda50c283cf087d3
X - Alias <73131745+Armaggon@users.noreply.github.com>
2022-02-17 18:57:10 +0000

    Update checker.py

--- checker.py
+++ checker.py
@@ -2,13 +2,6 @@
 import ftplib
 import sys
 
-
-# This tool is quite pointless all by itself, I made this purely as a fun small project when I had spare time.
-# It only takes 1min to test a target for ftp anonymous login so this is tool is best to implement/ utilize with another tool.
-
-# If you encounter the following error... "[Errno -2] Name or service not known"
-# That simply means that either the host you entered is bad, or port 21 is not open.
-
 def anonymous(ip):
     try:
         ftp=ftplib.FTP(ip)
